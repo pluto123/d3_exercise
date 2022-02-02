dba97a5080427a8af7a9d12d2ca8069e3c771665
Yuh-Chang Wu <yuh.chang.wu@gmail.com>
2022-02-02 18:50:04 +0800

    自訂刻度與標籤

--- d3_exercise_001/src/components/Demo07_2/index.js
+++ d3_exercise_001/src/components/Demo07_2/index.js
@@ -2,39 +2,35 @@ import * as d3 from 'd3'
 import { useEffect } from 'react';
 
 function makeDemo07_2() {
-    var sc = d3.scaleLinear().domain( [0,10] ).range( [0,200] );  //<1>
+    var sc = d3.scaleLinear().domain( [0,10] ).range( [0,200] );  // 輸入區間為 0 ~ 10，輸出像素範圍為 0 ~ 200
 
-    // top left: default settings
-    d3.select( "svg" ).append( "g" )                           //<2>
+    d3.select( "svg" ).append( "g" )
         .attr( "transform", "translate( 50,50)" )
         .call( d3.axisBottom(sc) );
     
-    // bottom left: additional decimal in labels
-    d3.select( "svg" ).append( "g" )                           //<3>
+    d3.select( "svg" ).append( "g" )
         .attr( "transform", "translate( 50,125)" )
-        .call( d3.axisBottom(sc).tickFormat( d3.format(".1f") ) )
+        .call( d3.axisBottom(sc).tickFormat( d3.format(".1f") ) )  // 刻度格式取小數點後一位
         .selectAll( "text" )
-        .filter( (d,i)=>i%2!=0 ).attr( "visibility", "hidden" );
+        .filter( (d,i)=>i%2!=0 ).attr( "visibility", "hidden" );  // 偶數刻度才顯示
 
-    // top right: minor and major tick marks, addtl label for axis
-    d3.select( "svg" ).append( "g" )                           //<4>
-        .attr( "transform", "translate(350,50)" )
-        .call( d3.axisBottom(sc).tickSize(3).tickFormat( ()=>"" ) );
     d3.select( "svg" ).append( "g" )
         .attr( "transform", "translate(350,50)" )
-        .call( d3.axisBottom( sc ).ticks(2) )
+        .call( d3.axisBottom(sc).tickFormat( ()=>"" ) );  // 消除刻度標籤
+    d3.select( "svg" ).append( "g" )
+        .attr( "transform", "translate(350,50)" )
+        .call( d3.axisBottom( sc ).ticks(2) )  // 分隔成 2 等份
         .append( "text" ).text( "Metric" )
         .attr( "x", sc(5) ).attr("y", 35 )
         .attr( "font-size", 12 ).attr( "fill", "black" );
     
-    // bottom right: custom appearance
-    var g = d3.select( "svg" ).append( "g" )                   //<5>
+    var g = d3.select( "svg" ).append( "g" )
         .attr( "transform", "translate(350,125)" )
         .call( d3.axisBottom(sc).tickPadding( 5 ) );
-    g.select( ".domain" ).attr( "visibility", "hidden" );
+    g.select( ".domain" ).attr( "visibility", "hidden" );  // 隱藏軸線
     g.selectAll( ".tick" ).select( "line" )
-        .attr( "stroke", "red" ).attr( "stroke-width", 2 );
-    g.selectAll( "text" ).attr( "font-size", 14 );
+        .attr( "stroke", "red" ).attr( "stroke-width", 2 );  // 改變刻度記號
+    g.selectAll( "text" ).attr( "font-size", 14 );  // 改變刻度標籤
 }
 
 function Demo07_2() {
